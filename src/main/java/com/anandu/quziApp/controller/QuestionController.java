14c1d890e0b3354f2e592c6c03d2823a4e3b81b6
ananthakrishna KP <anandu4879@gmail.com>
2024-12-14 16:26:23 +0530

    without lombok

--- src/main/java/com/anandu/quziApp/controller/QuestionController.java
+++ src/main/java/com/anandu/quziApp/controller/QuestionController.java
@@ -3,6 +3,9 @@ package com.anandu.quziApp.controller;
 import com.anandu.quziApp.Questions;
 import com.anandu.quziApp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,15 +18,15 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allquestion")
-    public List<Questions> getAllQuestion(){
+    public ResponseEntity<List<Questions>> getAllQuestion(){
         return questionService.getAllQuestion();
     }
     @GetMapping("category/{category}")
-    public List<Questions> getQuestionsbyCategory(@PathVariable String category){
+    public ResponseEntity<List<Questions>> getQuestionsbyCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
     @PostMapping("add")
-    public String addQuestion(@RequestBody Questions questions){
+    public ResponseEntity<String> addQuestion(@RequestBody Questions questions){
         return questionService.addQuestion(questions);
     }
 }
--- src/main/java/com/anandu/quziApp/service/QuestionService.java
+++ src/main/java/com/anandu/quziApp/service/QuestionService.java
@@ -3,25 +3,42 @@ package com.anandu.quziApp.service;
 import com.anandu.quziApp.Questions;
 import com.anandu.quziApp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
-    public List<Questions> getAllQuestion() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Questions>> getAllQuestion() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
-    public List<Questions> getQuestionsByCategory(String category) {
-        return questionDao.findBycategory(category);
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findBycategory(category), HttpStatus.OK);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
-    public String addQuestion(Questions questions) {
+    public ResponseEntity<String> addQuestion(Questions questions) {
          questionDao.save(questions);
-         return "Success";
+         return new ResponseEntity<>("Success",HttpStatus.CREATED);
 
     }
 }
